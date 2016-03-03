20b5089905a59ed5ec6b36403a654b9596e44af3
allenwhale <allenwhale@yahoo.com.tw>
2016-03-03 15:48:58 +0800

    add page table

--- src/js/components/pages/Home.jsx
+++ src/js/components/pages/Home.jsx
@@ -3,6 +3,12 @@ import FullWidthSection from '../FullWidthSection.jsx';
 import logo from '../../../assets/images/nctu.gif';
 import FontIcon from 'material-ui/lib/font-icon';
 import Colors from 'material-ui/lib/styles/colors';
+import Table from 'material-ui/lib/table/table';
+import TableHeaderColumn from 'material-ui/lib/table/table-header-column';
+import TableRow from 'material-ui/lib/table/table-row';
+import TableHeader from 'material-ui/lib/table/table-header';
+import TableRowColumn from 'material-ui/lib/table/table-row-column';
+import TableBody from 'material-ui/lib/table/table-body';
 
 class Home extends React.Component {
 
@@ -10,37 +16,60 @@ class Home extends React.Component {
     const iconStyles = {
       marginRight: 24,
     };
+    const data = [{
+        studentId: '0000000',
+        name: '王曉明',
+        postAccount: '0101010',
+        account: '1010101',
+        originDesk: 10,
+        originJobs: 10,
+        schedule: 20,
+        base: 10000,
+        salary: 130,
+        other: 100,
+        prev: 100000,
+        total: 100000,
+        pay: 1000000,
+        next: 1000000,
+    }]
+    const tableHeader = ['學號', '姓名', '局號', '帳號', '原始值班時數', '原始job時數', 'schedule時數', '基本薪', '時薪', 'other', '上月', '總結', '報支', '下月'];
+    console.log(tableHeader.map((col) => (<TableHeaderColumn>{col}</TableHeaderColumn>)));
     return (
           <FullWidthSection>
-            <h1>Simple home</h1>
-            <div>
-              <img className="img-thumbnail" src={logo} alt="LOGO"/>
-            </div>
-            <hr />
-            <FontIcon style={iconStyles} className="material-icons">
-            home
-            </FontIcon>
-            <FontIcon style={iconStyles} className="material-icons"
-              color={Colors.blue500}
-            >
-            home
-            </FontIcon>
-            <FontIcon
-              style={iconStyles} className="material-icons"
-              color={Colors.red500}
-              hoverColor={Colors.greenA200}
-            >home</FontIcon>
-            <hr />
-            <FontIcon className="material-icons" >home</FontIcon>
-            <FontIcon className="material-icons" color={Colors.red500}>
-            flight_takeoff
-            </FontIcon>
-            <FontIcon className="material-icons" color={Colors.yellow500}>
-            cloud_download
-            </FontIcon>
-            <FontIcon className="material-icons" color={Colors.blue500}>
-              videogame_asset
-            </FontIcon>
+          test
+            <Table>
+              <TableHeader displaySelectAll={false} adjustForCheckbox={false}>
+                <TableRow>
+                  {
+                    tableHeader.map((col) => (<TableHeaderColumn>{col}</TableHeaderColumn>))
+                  }
+                </TableRow>
+              </TableHeader>
+              <TableBody displayRowCheckbox={false}>
+                {
+                    data.map((row) => {
+                        return (
+                              <TableRow>
+                                <TableRowColumn>{row.studentId}</TableRowColumn>
+                                <TableRowColumn>{row.name}</TableRowColumn>
+                                <TableRowColumn>{row.postAccount}</TableRowColumn>
+                                <TableRowColumn>{row.account}</TableRowColumn>
+                                <TableRowColumn>{row.originDesk}</TableRowColumn>
+                                <TableRowColumn>{row.originJobs}</TableRowColumn>
+                                <TableRowColumn>{row.schedule}</TableRowColumn>
+                                <TableRowColumn>{row.base}</TableRowColumn>
+                                <TableRowColumn>{row.salary}</TableRowColumn>
+                                <TableRowColumn>{row.other}</TableRowColumn>
+                                <TableRowColumn>{row.prev}</TableRowColumn>
+                                <TableRowColumn>{row.total}</TableRowColumn>
+                                <TableRowColumn>{row.pay}</TableRowColumn>
+                                <TableRowColumn>{row.next}</TableRowColumn>
+                              </TableRow>
+                              );
+                    })
+                }
+              </TableBody>
+            </Table>
           </FullWidthSection>
           );
   }
